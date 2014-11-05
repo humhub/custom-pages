264b762712a9cc63f645ea5df7de2ccdf6d5cdc8
andystrobel <andreas.strobel@googlemail.com>
2014-11-05 16:42:55 +0100

    Built-in iFrame integration without full width

--- views/view/iframe.php
+++ views/view/iframe.php
@@ -1,7 +1,40 @@
+<?php
+
+// save url to standard loader graphic
+$loader_url = Yii::app()->baseUrl . "/img/loader.gif";
+
+// check if there is a themed loader graphic and replace the url
+if (file_exists(Yii::getPathOfAlias('webroot') . "/themes/" . Yii::app()->theme->name . "/img/loader.gif")) {
+    $loader_url = Yii::app()->theme->baseUrl . "/img/loader.gif";
+}
+
+?>
 
 <?php if ($navigationClass == CustomPage::NAV_CLASS_ACCOUNTNAV): ?>
-    
-    <b>TODO: Handle Iframe with not 100% width</b>
+
+    <iframe id="iframepage" style="width:100%; height: 400px;" src="<?php echo $url; ?>"></iframe>
+
+    <style>
+        #iframepage {
+            border: none;
+            background: url('<?php echo $loader_url; ?>') center center no-repeat;
+        }
+    </style>
+
+    <script>
+        window.onload = function (evt) {
+            setSize();
+        }
+        window.onresize = function (evt) {
+            setSize();
+        }
+
+        function setSize() {
+
+            $('#iframepage').css('height', window.innerHeight - 170 + 'px');
+        }
+    </script>
+
 
 <?php else: ?>
 
@@ -13,16 +46,16 @@
             left: 0;
             top: 98px;
             border: none;
-            background: url('loader.gif') center center no - repeat;
+            background: url('<?php echo $loader_url; ?>') center center no-repeat;
         }
-    </style>    
+    </style>
 
 
     <script>
-        window.onload = function(evt) {
+        window.onload = function (evt) {
             setSize();
         }
-        window.onresize = function(evt) {
+        window.onresize = function (evt) {
             setSize();
         }
 
@@ -31,5 +64,5 @@
             $('#iframepage').css('height', window.innerHeight - 100 + 'px');
             $('#iframepage').css('width', jQuery('body').outerWidth() - 1 + 'px');
         }
-    </script>    
+    </script>
 <?php endif; ?>

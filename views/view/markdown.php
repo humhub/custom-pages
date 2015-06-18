de403bf944edb802ba50c14ed69b859a9f52cb34
Robert Kolatzek <robert@kolatzek.org>
2015-06-18 10:05:31 +0200

    Wrap into div.custom_page_content
    for easy styling

--- views/view/markdown.php
+++ views/view/markdown.php
@@ -19,11 +19,12 @@
         <div class="row">
             <div class="panel panel-default">
                 <div class="panel-body">
-                    <?php
-                    $parser = new CMarkdownParser;
-                    echo $parser->safeTransform($md);
-                    ?>
-
+                    <div class="custom_page_content">
+                        <?php
+                        $parser = new CMarkdownParser;
+                        echo $parser->safeTransform($md);
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>

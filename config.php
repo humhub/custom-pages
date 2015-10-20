7f0efed8d7154dd2fb74fb1902c7916e39f226b5
Lucas Bartholemy <lucas@bartholemy.com>
2015-10-20 10:25:12 +0200

    V0.21 Compatibility

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 
 use humhub\modules\user\widgets\AccountMenu;
 use humhub\modules\admin\widgets\AdminMenu;
+use humhub\widgets\BaseMenu;
 use humhub\widgets\TopMenu;
 
 return [
@@ -13,7 +14,10 @@ return [
         ['class' => TopMenu::className(), 'event' => TopMenu::EVENT_INIT, 'callback' => ['humhub\modules\custom_pages\Events', 'onTopMenuInit']],
         ['class' => AccountMenu::className(), 'event' => AccountMenu::EVENT_INIT, 'callback' => ['humhub\modules\custom_pages\Events', 'onAccountMenuInit']],
         ['class' => humhub\modules\space\widgets\Menu::className(), 'event' => humhub\modules\space\widgets\Menu::EVENT_INIT, 'callback' => ['humhub\modules\custom_pages\Events', 'onSpaceMenuInit']],
+        // v0.20 and prior
         ['class' => humhub\modules\space\widgets\AdminMenu::className(), 'event' => humhub\modules\space\widgets\AdminMenu::EVENT_INIT, 'callback' => ['humhub\modules\custom_pages\Events', 'onSpaceAdminMenuInit']],
+        // v.021 and above
+        ['class' => 'humhub\modules\space\modules\manage\widgets\Menu', 'event' => BaseMenu::EVENT_INIT, 'callback' => ['humhub\modules\custom_pages\Events', 'onSpaceAdminMenuInit']],
     ],
 ];
 ?>

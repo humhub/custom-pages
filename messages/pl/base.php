4b9615b5bf19701403d8063e6183778237864e4c
HumHub Translate <info@humhub.com>
2016-03-09 14:53:57 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/base.php
+++ messages/pl/base.php
@@ -13,5 +13,5 @@ return array (
   'Top Navigation' => 'Górna nawigacja',
   'Type' => 'Rodzaj',
   'User Account Menu (Settings)' => 'Konto użytkownika (Ustawienia)',
-  'Without adding to navigation (Direct link)' => '',
+  'Without adding to navigation (Direct link)' => 'Bez dodawania do nawigacji (odnośnik bezpośredni)',
 );
--- messages/pl/views_admin_edit.php
+++ messages/pl/views_admin_edit.php
@@ -1,12 +1,12 @@
 <?php
 return array (
-  '<strong>Create</strong> page' => '',
-  '<strong>Edit</strong> page' => '',
-  'Content' => '',
-  'Default sort orders scheme: 100, 200, 300, ...' => '',
+  '<strong>Create</strong> page' => '<strong>Utwórz</strong> stronę',
+  '<strong>Edit</strong> page' => '<strong>Edytuj</strong> stronę',
+  'Content' => 'Zawartość',
+  'Default sort orders scheme: 100, 200, 300, ...' => 'Domyślna kolejność sortowania: 100, 200, 300, ...',
   'Delete' => 'Usuń',
-  'Page title' => '',
+  'Page title' => 'Tytuł strony',
   'Save' => 'Zapisz ',
   'Sort Order' => 'Sortuj',
-  'URL' => '',
+  'URL' => 'URL',
 );

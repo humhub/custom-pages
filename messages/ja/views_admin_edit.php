5e16fd60589b43c7b67bbd9217edbdd4e712e3ee
HumHub Translate <info@humhub.com>
2016-04-14 13:36:30 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/views_admin_edit.php
+++ messages/ja/views_admin_edit.php
@@ -1,12 +1,12 @@
 <?php
 return array (
-  '<strong>Create</strong> page' => '',
-  '<strong>Edit</strong> page' => '',
-  'Content' => '',
-  'Default sort orders scheme: 100, 200, 300, ...' => '',
+  '<strong>Create</strong> page' => '<strong>作成し</strong> ページ',
+  '<strong>Edit</strong> page' => '<strong>編集</strong> ページ',
+  'Content' => 'コンテンツ',
+  'Default sort orders scheme: 100, 200, 300, ...' => '既定の並べ替えの注文方式: 100 200 300...',
   'Delete' => '削除',
-  'Page title' => '',
+  'Page title' => 'ページのタイトル',
   'Save' => '保存',
-  'Sort Order' => '',
-  'URL' => '',
+  'Sort Order' => '並べ替え順序',
+  'URL' => 'URL',
 );

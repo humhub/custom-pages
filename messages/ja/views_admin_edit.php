f0ceff17c703a75b6cb2509d1ae0ca6d8f495bcc
Sarah Tsumayoi <Felli@users.noreply.github.com>
2016-04-15 05:25:52 -0400

    Fix: humhub-bot breaking translation
    This fixes what the humhub-bot broke. Might want to look over any other updates that humhub-bot did for /messahes/ja as well. xD

--- messages/ja/views_admin_edit.php
+++ messages/ja/views_admin_edit.php
@@ -2,11 +2,7 @@
 return array (
   '<strong>Create</strong> page' => '<strong>作成し</strong> ページ',
   '<strong>Edit</strong> page' => '<strong>編集</strong> ページ',
-<<<<<<< HEAD
   'Content' => 'コンテンツ',
-=======
-  'Content' => '',
->>>>>>> b0393490736e5dbc48f692a1ba00f30b2396de93
   'Default sort orders scheme: 100, 200, 300, ...' => '既定の並べ替えの注文方式: 100 200 300...',
   'Delete' => '削除',
   'Page title' => 'ページのタイトル',

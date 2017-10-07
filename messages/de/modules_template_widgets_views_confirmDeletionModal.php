8c6229c7f6cffa1421a97d5d91d07af19771177c
HumHub Translations <info@humhub.com>
2017-10-07 11:26:56 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/modules_template_widgets_views_confirmDeletionModal.php
+++ messages/de/modules_template_widgets_views_confirmDeletionModal.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 001101
- * Date: 25.01.2017
- * Time: 02:17
- */
 return array (
-    'Are you sure you want to delete this container item?' => 'Willst du dieses Container Element wirklich löschen?',
+  'Are you sure you want to delete this container item?' => 'Willst du dieses Container Element wirklich löschen?',
 );

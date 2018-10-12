c63cc18b4bb495ec5c381b4f9accc8111a61be88
HumHub Translations <info@humhub.com>
2018-10-12 12:57:13 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/controllers_TemplateController.php
+++ messages/it/controllers_TemplateController.php
@@ -2,7 +2,7 @@
 return array (
   '<strong>Edit</strong> {type} element' => '<strong>Modifca</strong> {type} elemento',
   'Access denied!' => 'Accesso non consentito!',
-  'Empty content elements cannot be delted!' => '',
+  'Empty content elements cannot be delted!' => 'Gli elementi di contenuto vuoti non possono essere eliminati!',
   'Invalid request data!' => 'Richiesta non valida!',
   'You are not allowed to delete default content!' => 'Non sei abilitato a cancellare il contenuto predefinito!',
 );
--- messages/it/models_SettignsForm.php
+++ messages/it/models_SettignsForm.php
@@ -2,9 +2,9 @@
 return array (
   'Activate PHP based Pages and Snippets' => 'Attiva Pagine e Sippets basati su PHP',
   'If disabled, existing php pages will still be online, but can\'t be created.' => 'Se disattivato, le pagine php esistenti resteranno online ma non potranno essere create.',
-  'PHP view path for custom space pages' => '',
-  'PHP view path for custom space snippets' => '',
-  'PHP view path for global custom pages' => '',
-  'PHP view path for global custom snippets' => '',
-  'The given view file path does not exist.' => '',
+  'PHP view path for custom space pages' => 'Percorso delle view PHP per pagine personalizzate degli spazi ',
+  'PHP view path for custom space snippets' => 'Percorso delle view PHP per pagine personalizzate degli snippet degli spazi ',
+  'PHP view path for global custom pages' => 'Percorso delle view PHP per pagine personalizzate globali',
+  'PHP view path for global custom snippets' => 'Percorso delle view PHP per pagine personalizzate degli snippet',
+  'The given view file path does not exist.' => 'Il percorso del file della view non esiste.',
 );

d896adcef47ebc1c8aeb3b39b7b5e6e61e54cd20
HumHub Translations <info@humhub.com>
2018-11-22 13:47:44 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/modules_template_views_admin_editSource.php
+++ messages/hu/modules_template_views_admin_editSource.php
@@ -1,9 +1,8 @@
 <?php
-
-return [
-    'Add Element' => '',
-    'Edit All' => '',
-    'Edit template \'{templateName}\'' => '',
-    'Here you can edit the source of your template by defining the template layout and adding content elements. Each element can be assigned with a default content and additional definitions.' => '',
-    'You haven\'t saved your last changes yet. Do you want to leave without saving?' => '',
-];
+return array (
+  'Add Element' => 'Elem Hozzáadása',
+  'Edit All' => 'Az összes szerkesztése',
+  'Edit template \'{templateName}\'' => '',
+  'Here you can edit the source of your template by defining the template layout and adding content elements. Each element can be assigned with a default content and additional definitions.' => '',
+  'You haven\'t saved your last changes yet. Do you want to leave without saving?' => 'Még nem mentetted el az utóbbi módosításokat. Biztos, hogy elhagyod az oldalt, mentés nélkül?',
+);

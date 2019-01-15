4c88e67252d65cc85f0f004ae3cfbf1a3aadd658
buddh4 <mail@jharrer.de>
2019-01-15 17:39:23 +0100

    Fixed translation espape issue.

--- modules/template/views/admin/info.php
+++ modules/template/views/admin/info.php
@@ -21,10 +21,14 @@ use yii\widgets\ActiveForm;
                             . 'You can add elements as Richtexts or Images into your template by using the \'Add Element\' dropdown menu.<br />'
                             . 'After adding an element, the elements placeholder is automatically inserted to your template.<br />'
                             . 'You can change the position of your elements anytime. The element for an block with the name \'content\' '
-                            . 'will be represented as \'{{ content }}\' within your template.<br /><br /> The name of your block hast to start with an '
+                            . 'will be represented as {contentVar} within your template.<br /><br /> The name of your block hast to start with an '
                             . 'alphanumeric letter and cannot contain any special signs except an \'_\'. '
                             . 'Each element provides additional placeholder for rendering the default content or edit links. '
-                            . 'These additions can be inserted adding for example {{ content.default }} to your template.', ['twigLink' => '<strong><a href="http://twig.sensiolabs.org/">Twig</a></strong>']); ?>
+                            . 'These additions can be inserted adding for example {contentDefaultVar} to your template.', [
+                                    'twigLink' => '<strong><a href="http://twig.sensiolabs.org/">Twig</a></strong>',
+                        'contentVar' => '{{ content }}',
+                        'contentDefaultVar' => '{{ content.default }}'
+                        ]); ?>
                     <br /><br />
                     <?= Yii::t('CustomPagesModule.modules_template_views_admin_info', 'More infos about the twig syntax is available <strong><a href="{twig_tmpl_url}">here</a></strong>', ['twig_tmpl_url' => 'http://twig.sensiolabs.org/doc/templates.html']); ?>
                 </div>

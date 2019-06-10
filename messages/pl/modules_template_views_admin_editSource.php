50b3efe526cc109ae0d0b49ec25124460bcdf7fc
HumHub Translations <info@humhub.com>
2019-06-10 20:22:13 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/modules_template_views_admin_editSource.php
+++ messages/pl/modules_template_views_admin_editSource.php
@@ -1,9 +1,8 @@
 <?php
-
-return [
-    'Add Element' => '',
-    'Edit All' => '',
-    'Edit template \'{templateName}\'' => '',
-    'Here you can edit the source of your template by defining the template layout and adding content elements. Each element can be assigned with a default content and additional definitions.' => '',
-    'You haven\'t saved your last changes yet. Do you want to leave without saving?' => '',
-];
+return array (
+  'Add Element' => 'Dodaj element',
+  'Edit All' => 'Edytuj Wszystkie',
+  'Edit template \'{templateName}\'' => 'Edytuj szablon \'{templateName}\'',
+  'Here you can edit the source of your template by defining the template layout and adding content elements. Each element can be assigned with a default content and additional definitions.' => 'Tutaj możesz edytować źródło twojego szablonu ustalając układ szablonu i dodawać elementy z treścią. Każdy element może być przypisany z domyślną zawartością i dodatkowymi funkcjami.',
+  'You haven\'t saved your last changes yet. Do you want to leave without saving?' => 'Nie zapisałeś zmian. Czy chcesz wyjść bez zapisu?',
+);

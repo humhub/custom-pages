155849a8f603d48ed3420b8ad5fd938b94f87c39
HumHub Translations <info@humhub.com>
2019-06-19 15:28:21 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/models_ContainerPage.php
+++ messages/pl/models_ContainerPage.php
@@ -2,5 +2,5 @@
 return array (
   'Only visible for space admins' => 'Widoczne tylko dla administratorów strefy',
   'Open in new window' => 'Otwórz w nowym oknie',
-  'page' => 'strona',
+  'page' => 'strony',
 );
--- messages/pl/models_ContainerSnippet.php
+++ messages/pl/models_ContainerSnippet.php
@@ -1,4 +1,4 @@
 <?php
 return array (
-  'snippet' => 'fragment',
+  'snippet' => 'fragmentu',
 );

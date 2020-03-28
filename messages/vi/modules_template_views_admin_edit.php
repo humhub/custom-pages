7e608ffa2f40feb42a0ae5da4a64b748ef7838be
HumHub Translations <info@humhub.com>
2020-03-28 12:57:15 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/vi/modules_template_views_admin_edit.php
+++ messages/vi/modules_template_views_admin_edit.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'Create new {type}' => '',
-  'Edit template \'{templateName}\'' => '',
+  'Create new {type}' => 'Tạo {type} mới',
+  'Edit template \'{templateName}\'' => 'Sửa mẫu \'{templateName}\'',
   'Save' => 'Lưu',
 );
--- messages/vi/modules_template_views_admin_editSource.php
+++ messages/vi/modules_template_views_admin_editSource.php
@@ -1,9 +1,8 @@
 <?php
-
-return [
-    'Add Element' => '',
-    'Edit All' => '',
-    'Edit template \'{templateName}\'' => '',
-    'Here you can edit the source of your template by defining the template layout and adding content elements. Each element can be assigned with a default content and additional definitions.' => '',
-    'You haven\'t saved your last changes yet. Do you want to leave without saving?' => '',
-];
+return array (
+  'Add Element' => 'Add Element',
+  'Edit All' => 'Edit All',
+  'Edit template \'{templateName}\'' => 'Edit template \'{templateName}\'',
+  'Here you can edit the source of your template by defining the template layout and adding content elements. Each element can be assigned with a default content and additional definitions.' => 'Here you can edit the source of your template by defining the template layout and adding content elements. Each element can be assigned with a default content and additional definitions.',
+  'You haven\'t saved your last changes yet. Do you want to leave without saving?' => 'You haven\'t saved your last changes yet. Do you want to leave without saving?',
+);

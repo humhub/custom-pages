5bfd81a27a0a85b688043733c51ceedd7ff6f83a
MasterMindNET <romazahrypa@gmail.com>
2020-06-24 17:00:55 +0300

    fix: in 'messages' folder files with wrong tag '</ b>' replaced with the correct one ('</b>');

--- messages/pt_br/views_common_edit.php
+++ messages/pt_br/views_common_edit.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'By setting an url shortcut value, you can create a better readable url for your page. If <b>URL Rewriting</b> is enabled on your site, the value \'mypage\' will result in an url \'www.example.de/p/mypage\'.' => 'Ao definir um valor de atalho de URL, você pode criar um URL mais legível para sua página. Se <b> Regravação de URL </ b> estiver ativada em seu site, o valor "mypage" resultará em um URL "www.example.de/p/mypage".',
+  'By setting an url shortcut value, you can create a better readable url for your page. If <b>URL Rewriting</b> is enabled on your site, the value \'mypage\' will result in an url \'www.example.de/p/mypage\'.' => 'Ao definir um valor de atalho de URL, você pode criar um URL mais legível para sua página. Se <b> Regravação de URL </b> estiver ativada em seu site, o valor "mypage" resultará em um URL "www.example.de/p/mypage".',
   'Configuration' => 'Configuração',
   'Delete' => 'Deletar',
   'Here you can configure the general settings of your {label}.' => 'Aqui você pode definir as configurações gerais do seu {label}.',

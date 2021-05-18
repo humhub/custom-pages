6a7f16d7338f2cf92e91dddae4b32f0f3cbde496
HumHub Translations <info@humhub.com>
2021-05-18 20:26:25 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/views_common_edit.php
+++ messages/pl/views_common_edit.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'By setting an url shortcut value, you can create a better readable url for your page. If <b>URL Rewriting</b> is enabled on your site, the value \'mypage\' will result in an url \'www.example.de/p/mypage\'.' => 'Ustawiając skróconą wersję adresu, możesz stworzyć przejrzysty adres strony. Jeżeli <b>Przepisywanie adresu</b> jest aktywne na stronie, wartość \'mojastrona\' da adres \'www.przyklad.pl/p/mojastrona\'.',
-    'Configuration' => 'Konfiguracja',
-    'Delete' => 'Usuń',
-    'Inline Editor' => 'Edytor w linii',
-    'e.g. http://www.example.de' => 'n.p. http://www.przyklad.pl',
-    '<strong>Info: </strong> Pages marked as "Admin Only" are not shown in the stream!' => '',
-    'Here you can configure the general settings of your {pageLabel}.' => '',
-    'The abstract will be used as stream entry content to promote the actual page. 
-                        If no abstract is given or the page is only visible for admins, no stream entry will be created.' => '',
-];
+return array (
+  '<strong>Info: </strong> Pages marked as "Admin Only" are not shown in the stream!' => '<strong>Informacja: </strong> Strony oznaczone jako "Tylko Administrator" nie są widoczne w strumieniu!',
+  'By setting an url shortcut value, you can create a better readable url for your page. If <b>URL Rewriting</b> is enabled on your site, the value \'mypage\' will result in an url \'www.example.de/p/mypage\'.' => 'Ustawiając skróconą wersję adresu, możesz stworzyć przejrzysty adres strony. Jeżeli <b>Przepisywanie adresu</b> jest aktywne na stronie, wartość \'mojastrona\' da adres \'www.przyklad.pl/p/mojastrona\'.',
+  'Configuration' => 'Konfiguracja',
+  'Delete' => 'Usuń',
+  'Here you can configure the general settings of your {pageLabel}.' => 'Tutaj możesz skonfigurować ustawienia {pageLabel}.',
+  'Inline Editor' => 'Edytor w linii',
+  'The abstract will be used as stream entry content to promote the actual page. 
+                        If no abstract is given or the page is only visible for admins, no stream entry will be created.' => 'Widok zostanie użyty jako wejściowy wpis strumienia w celu promowania właściwej strony. Jeśli żaden widok nie zostanie podany lub strona jest widoczna tylko dla administratorów, żaden wpis strumienia nie zostanie utworzony.',
+  'e.g. http://www.example.de' => 'n.p. http://www.przyklad.pl',
+);

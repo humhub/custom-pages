e6f9f28f4fe69d76b00feab5466ea47d020e766f
HumHub Translations <info@humhub.com>
2021-06-11 10:23:36 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/el/models_Page.php
+++ messages/el/models_Page.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Navigation' => 'Πλοήγηση',
-    'Only visible for admins' => 'Ορατό μόνο για διαχειριστές',
-    'Open in new window' => 'Άνοιγμα σε νέο παράθυρο',
-    'Url shortcut' => 'συντόμευση Url',
-    'View' => 'Θέα',
-    'page' => 'Σελίδα',
-    'Abstract' => '',
-    'Page' => '',
-];
+return array (
+  'Abstract' => 'Σύνοψη',
+  'Navigation' => 'Πλοήγηση',
+  'Only visible for admins' => 'Ορατό μόνο για διαχειριστές',
+  'Open in new window' => 'Άνοιγμα σε νέο παράθυρο',
+  'Page' => 'Σελίδα',
+  'Url shortcut' => 'συντόμευση Url',
+  'View' => 'Θέα',
+  'page' => 'Σελίδα',
+);

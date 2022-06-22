9254ea77e4debb182df08c7bc7e1f973fb8cd73d
Yura Bakhtin <yurybakh@gmail.com>
2022-06-22 07:20:17 +0300

    Fix undefined tinymce on pjax loading

--- assets/TinyMcePluginsAssets.php
+++ assets/TinyMcePluginsAssets.php
@@ -8,7 +8,6 @@
 namespace humhub\modules\custom_pages\assets;
 
 use humhub\components\assets\AssetBundle;
-use humhub\modules\ui\view\components\View;
 
 class TinyMcePluginsAssets extends AssetBundle
 {
@@ -16,16 +15,4 @@ class TinyMcePluginsAssets extends AssetBundle
      * @inheritdoc
      */
     public $sourcePath = '@custom_pages/resources/tinymce/plugins';
-
-    /**
-     * @inheritdoc
-     */
-    public $js = [
-        'codemirror/plugin.min.js'
-    ];
-
-    /**
-     * @inheritdoc
-     */
-    public $jsOptions = ['position' => View::POS_END];
 }

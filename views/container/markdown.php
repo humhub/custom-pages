39b63f7f60b88f9399f01c79952993fd95d59ec5
Marc Farré <contact@marc.fun>
2022-09-21 16:20:11 +0200

    Fix #244: Add markdown-render class to Markdown text for Translator module to work

--- views/container/markdown.php
+++ views/container/markdown.php
@@ -1,12 +1,13 @@
 <?php
 
+use humhub\modules\content\widgets\richtext\RichText;
 use humhub\modules\custom_pages\modules\template\widgets\PageConfigurationButton;
 use humhub\modules\custom_pages\widgets\CustomPageInlineStyle;
 use yii\helpers\Html;
-use humhub\modules\content\widgets\richtext\RichText;
+
 /** @var $page \humhub\modules\custom_pages\models\Page */
 
-$cssClass = ($page->hasAttribute('cssClass') && !empty($page->cssClass)) ? $page->cssClass :  'custom-pages-page';
+$cssClass = ($page->hasAttribute('cssClass') && !empty($page->cssClass)) ? $page->cssClass : 'custom-pages-page';
 ?>
 
 <?= CustomPageInlineStyle::widget(['theme' => $this->theme]); ?>
@@ -14,6 +15,8 @@ $cssClass = ($page->hasAttribute('cssClass') && !empty($page->cssClass)) ? $page
 <?= PageConfigurationButton::widget() ?>
 <div class="panel panel-default <?= Html::encode($cssClass) ?>">
     <div class="panel-body">
-        <?= RichText::output($md)?>
+        <div class="markdown-render">
+            <?= RichText::output($md) ?>
+        </div>
     </div>
 </div>

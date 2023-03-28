4c1c486ca4f98b5fcd87670dcc0cee241cef18d5
Yuriy Bakhtin <yurybakh@gmail.com>
2023-03-28 13:46:27 +0300

    Fix tests

--- tests/codeception/fixtures/data/content.php
+++ tests/codeception/fixtures/data/content.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    ['guid' => 'e0acdc54-13c8-4778-adc1-7302eabce100', 'object_model' => 'humhub\modules\custom_pages\models\Page', 'object_id' => 1, 'visibility' => '0', 'pinned' => '0', 'archived' => '0', 'contentcontainer_id' => 4, 'created_at' => '2021-05-14 14:40:00', 'created_by' => '2', 'updated_at' => '2021-05-14 14:40:00', 'updated_by' => '2', 'stream_channel' => 'activity'],
-    ['guid' => 'e0acdc54-13c8-4778-adc1-7302eabce101', 'object_model' => 'humhub\modules\custom_pages\models\Page', 'object_id' => 2, 'visibility' => '0', 'pinned' => '0', 'archived' => '0', 'contentcontainer_id' => 4, 'created_at' => '2021-05-14 14:40:00', 'created_by' => '3', 'updated_at' => '2021-05-14 14:40:00', 'updated_by' => '3', 'stream_channel' => 'activity'],
+    ['guid' => 'e0acdc54-13c8-4778-adc1-7302eabce100', 'object_model' => 'humhub\modules\custom_pages\models\Page', 'object_id' => 1, 'created_at' => '2023-03-28 10:40:00', 'created_by' => '2', 'updated_at' => '2023-03-28 10:40:00', 'updated_by' => '2'],
+    ['guid' => 'e0acdc54-13c8-4778-adc1-7302eabce101', 'object_model' => 'humhub\modules\custom_pages\models\Page', 'object_id' => 2, 'created_at' => '2023-03-28 10:40:00', 'created_by' => '3', 'updated_at' => '2023-03-28 10:40:00', 'updated_by' => '3'],
 ];

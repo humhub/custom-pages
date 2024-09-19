145210200f64027510f981ed265f09d3f4fefff8
Yuriy Bakhtin <yurybakh@gmail.com>
2024-09-19 11:23:37 +0200

    Fix module ID

--- tests/codeception/acceptance/CreatePhpPageCest.php
+++ tests/codeception/acceptance/CreatePhpPageCest.php
@@ -21,10 +21,10 @@ class CreatePhpPageCest
 
         $I->wait(1);
 
-        $I->fillField('SettingsForm[phpGlobalPagePath]', '@custom_pages/tests/codeception/_data/views/global_pages');
-        $I->fillField('SettingsForm[phpGlobalSnippetPath]', '@custom_pages/tests/codeception/_data/views/global_snippets');
-        $I->fillField('SettingsForm[phpContainerSnippetPath]', '@custom_pages/tests/codeception/_data/views/container_snippets');
-        $I->fillField('SettingsForm[phpContainerPagePath]', '@custom_pages/tests/codeception/_data/views/container_pages');
+        $I->fillField('SettingsForm[phpGlobalPagePath]', '@custom-pages/tests/codeception/_data/views/global_pages');
+        $I->fillField('SettingsForm[phpGlobalSnippetPath]', '@custom-pages/tests/codeception/_data/views/global_snippets');
+        $I->fillField('SettingsForm[phpContainerSnippetPath]', '@custom-pages/tests/codeception/_data/views/container_snippets');
+        $I->fillField('SettingsForm[phpContainerPagePath]', '@custom-pages/tests/codeception/_data/views/container_pages');
         $I->click('Save');
 
         $I->seeSuccess('Saved');

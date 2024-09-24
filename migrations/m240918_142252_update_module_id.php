fa96a5452ddb61b5cb72f3424e48d43463379e7d
Yuriy Bakhtin <yurybakh@gmail.com>
2024-09-24 16:24:05 +0200

    Fix module ID in other tables

--- migrations/m240918_142252_update_module_id.php
+++ migrations/m240918_142252_update_module_id.php
@@ -2,6 +2,9 @@
 
 use humhub\components\Migration;
 use humhub\models\ModuleEnabled;
+use humhub\models\Setting;
+use humhub\modules\activity\models\Activity;
+use humhub\modules\content\models\ContentContainerSetting;
 
 /**
  * Class m240918_142252_update_module_id
@@ -13,10 +16,17 @@ class m240918_142252_update_module_id extends Migration
      */
     public function safeUp()
     {
-        $moduleEnabled = ModuleEnabled::findOne(['module_id' => 'custom_pages']);
+        $oldId = 'custom_pages';
+        $newId = 'custom-pages';
+
+        $moduleEnabled = ModuleEnabled::findOne(['module_id' => $oldId]);
         if ($moduleEnabled) {
-            $moduleEnabled->module_id = 'custom-pages';
+            $moduleEnabled->module_id = $newId;
             $moduleEnabled->save();
+
+            Activity::updateAll(['module' => $newId], ['module' => $oldId]);
+            Setting::updateAll(['module_id' => $newId], ['module_id' => $oldId]);
+            ContentContainerSetting::updateAll(['module_id' => $newId], ['module_id' => $oldId]);
         }
     }
 

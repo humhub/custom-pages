4bd0d3125a73e76da2edbdd4280d1f7e6c5bbb27
Lucas Bartholemy <lucas@bartholemy.com>
2025-06-16 21:20:39 +0200

    Updated Manual

--- modules/template/elements/BaseContentRecordElementVariable.php
+++ modules/template/elements/BaseContentRecordElementVariable.php
@@ -36,9 +36,10 @@ class BaseContentRecordElementVariable extends BaseRecordElementVariable
     public bool $isDraft;
     public bool $isScheduled;
     public bool $isDeleted;
+    public string $url;
 
-    public ?UserElementVariable $_author = null;
-    public ?UserElementVariable $_updater = null;
+    public ?UserElementVariable $_createdBy = null;
+    public ?UserElementVariable $_updatedBy = null;
     public SpaceElementVariable|UserElementVariable|null $_container = null;
 
     public function __construct(BaseElementContent $elementContent)
@@ -70,29 +71,31 @@ class BaseContentRecordElementVariable extends BaseRecordElementVariable
             $this->isDraft = (bool) $service->isDraft();
             $this->isScheduled = (bool) $service->isScheduled();
             $this->isDeleted = (bool) $service->isDeleted();
+
+            $this->url = $record->content->getUrl();
         }
 
         return parent::setRecord($record);
     }
 
-    public function getAuthor(): ?UserElementVariable
+    public function getCreatedBy(): ?UserElementVariable
     {
-        if ($this->_author === null && $this->record instanceof ContentActiveRecord) {
-            $this->_author = UserElementVariable::instance($this->elementContent)
+        if ($this->_createdBy === null && $this->record instanceof ContentActiveRecord) {
+            $this->_createdBy = UserElementVariable::instance($this->elementContent)
                 ->setRecord($this->record->content->createdBy);
         }
 
-        return $this->_author;
+        return $this->_createdBy;
     }
 
-    public function getUpdater(): ?UserElementVariable
+    public function getUpdatedBy(): ?UserElementVariable
     {
-        if ($this->_updater === null && $this->record instanceof ContentActiveRecord) {
-            $this->_updater = UserElementVariable::instance($this->elementContent)
+        if ($this->_updatedBy === null && $this->record instanceof ContentActiveRecord) {
+            $this->_updatedBy = UserElementVariable::instance($this->elementContent)
                 ->setRecord($this->record->content->updatedBy);
         }
 
-        return $this->_updater;
+        return $this->_updatedBy;
     }
 
     public function getContainer(): UserElementVariable|SpaceElementVariable|null

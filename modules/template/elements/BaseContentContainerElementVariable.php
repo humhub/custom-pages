7f6eabdc12fcbe6063379160bd64004307d58f17
Yuriy Bakhtin <yurybakh@gmail.com>
2025-06-16 14:54:21 +0200

    Add static properties for Twig Template Variables

--- modules/template/elements/BaseContentContainerElementVariable.php
+++ modules/template/elements/BaseContentContainerElementVariable.php
@@ -14,15 +14,23 @@ use yii\db\ActiveRecord;
 class BaseContentContainerElementVariable extends BaseRecordElementVariable
 {
     public string $displayName;
+    public string $displayNameSub;
     public string $url;
     public string $guid;
+    public string $imageUrl;
+    public string $bannerImageUrl;
+    public array $tags;
 
     public function setRecord(?ActiveRecord $record): BaseRecordElementVariable
     {
         if ($record instanceof ContentContainerActiveRecord) {
             $this->displayName = $record->displayName;
+            $this->displayNameSub = $record->displayNameSub;
             $this->url = $record->getUrl();
             $this->guid = $record->guid;
+            $this->imageUrl = $record->profileImage->getUrl();
+            $this->bannerImageUrl = $record->profileBannerImage->getUrl();
+            $this->tags = $record->tags;
         }
 
         return parent::setRecord($record);
--- modules/template/elements/BaseContentRecordElementVariable.php
+++ modules/template/elements/BaseContentRecordElementVariable.php
@@ -10,24 +10,105 @@ namespace humhub\modules\custom_pages\modules\template\elements;
 
 use humhub\modules\content\components\ContentActiveRecord;
 use humhub\modules\custom_pages\lib\templates\twig\TwigEngine;
+use humhub\modules\space\models\Space;
+use humhub\modules\user\models\User;
+use yii\db\ActiveRecord;
 
+/**
+ * @property-read UserElementVariable $author
+ * @property-read UserElementVariable $updater
+ * @property-read SpaceElementVariable|UserElementVariable $container
+ */
 class BaseContentRecordElementVariable extends BaseRecordElementVariable
 {
+    public string $guid;
+    public string $createdAt;
+    public string $updatedAt;
+
+    public bool $isArchived;
+    public bool $isPinned;
+    public bool $isHidden;
+    public bool $isPublic;
+    public bool $isPrivate;
+    public bool $lockedComments;
+
+    public bool $isPublished;
+    public bool $isDraft;
+    public bool $isScheduled;
+    public bool $isDeleted;
+
     public ?UserElementVariable $_author = null;
+    public ?UserElementVariable $_updater = null;
+    public SpaceElementVariable|UserElementVariable|null $_container = null;
 
     public function __construct(BaseElementContent $elementContent)
     {
         parent::__construct($elementContent);
-        TwigEngine::registerSandboxExtensionAllowedFunctions(static::class, ['getAuthor']);
+        TwigEngine::registerSandboxExtensionAllowedFunctions(static::class, [
+            'getAuthor',
+            'getUpdater',
+            'getContainer',
+        ]);
+    }
+
+    public function setRecord(?ActiveRecord $record): BaseRecordElementVariable
+    {
+        if ($record instanceof ContentActiveRecord) {
+            $this->guid = $record->content->guid;
+            $this->createdAt = $record->content->created_at;
+            $this->updatedAt = $record->content->updated_at;
+
+            $this->isArchived = (bool) $record->content->archived;
+            $this->isPinned = (bool) $record->content->pinned;
+            $this->isHidden = (bool) $record->content->hidden;
+            $this->isPublic = (bool) $record->content->isPublic();
+            $this->isPrivate = (bool) $record->content->isPrivate();
+            $this->lockedComments = (bool) $record->content->locked_comments;
+
+            $service = $record->content->getStateService();
+            $this->isPublished = (bool) $service->isPublished();
+            $this->isDraft = (bool) $service->isDraft();
+            $this->isScheduled = (bool) $service->isScheduled();
+            $this->isDeleted = (bool) $service->isDeleted();
+        }
+
+        return parent::setRecord($record);
     }
 
     public function getAuthor(): ?UserElementVariable
     {
         if ($this->_author === null && $this->record instanceof ContentActiveRecord) {
             $this->_author = UserElementVariable::instance($this->elementContent)
-                ->setRecord($this->record->createdBy);
+                ->setRecord($this->record->content->createdBy);
         }
 
         return $this->_author;
+    }
+
+    public function getUpdater(): ?UserElementVariable
+    {
+        if ($this->_updater === null && $this->record instanceof ContentActiveRecord) {
+            $this->_updater = UserElementVariable::instance($this->elementContent)
+                ->setRecord($this->record->content->updatedBy);
+        }
+
+        return $this->_updater;
+    }
+
+    public function getContainer(): UserElementVariable|SpaceElementVariable|null
+    {
+        if ($this->_container === null && $this->record instanceof ContentActiveRecord) {
+            $container = $this->record->content->container;
+            if ($container instanceof Space) {
+                $this->_container = SpaceElementVariable::instance($this->elementContent);
+            } elseif ($container instanceof User) {
+                $this->_container = UserElementVariable::instance($this->elementContent);
+            } else {
+                return null;
+            }
+            $this->_container->setRecord($this->record->content->container);
+        }
+
+        return $this->_container;
     }
 }

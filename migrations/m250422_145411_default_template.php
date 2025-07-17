234f2edbfda697f81f4588ca5e631b6415e3a1e4
Yuriy Bakhtin <yurybakh@gmail.com>
2025-07-17 13:22:03 +0300

    Fix tests

--- migrations/m250422_145411_default_template.php
+++ migrations/m250422_145411_default_template.php
@@ -11,6 +11,11 @@ class m250422_145411_default_template extends Migration
     public function safeUp()
     {
         $this->safeAddColumn('custom_pages_template', 'is_default', $this->boolean()->notNull()->defaultValue(0));
+
+        // Need to add the columns here to avoid errors on work with class Template
+        $this->safeAddColumn('custom_pages_template', 'css', $this->text()->after('source'));
+        $this->safeAddColumn('custom_pages_template', 'js', $this->text()->after('css'));
+
         TemplateImportService::instance()->importDefaultTemplates();
     }
 
